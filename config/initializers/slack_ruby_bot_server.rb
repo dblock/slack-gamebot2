29042f688c3aa99e1ff377e39a42a1254f716291
Daniel (dB.) Doubrovkine <dblock@dblock.org>
2024-06-27 10:29:49 -0400

    Added missing history scopes.

--- config/initializers/slack_ruby_bot_server.rb
+++ config/initializers/slack_ruby_bot_server.rb
@@ -2,8 +2,10 @@ SlackRubyBotServer.configure do |config|
   config.oauth_version = :v2
   config.oauth_scope = [
     'app_mentions:read',
+    'channels:history',
     'channels:read',
     'chat:write',
+    'groups:history',
     'groups:read',
     'im:history',
     'im:write',
